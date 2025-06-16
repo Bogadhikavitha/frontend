8913a7fc767355188b3c6d24ac021ccfa6942dee
Bogadhikavitha <Bogadhikavitha@gmail.com>
2025-06-16 16:05:54 +0530

    Add files via upload

---
+++ opeartors/Rel.java
@@ -0,0 +1,14 @@
+class Rel
+{
+public static void main(String args[])
+{
+int a=2;
+int b=4;
+ System.out.println(a<b);
+System.out.println(a>b);
+System.out.println(a<=b);
+System.out.println(a>=b);
+System.out.println(a%=b);
+System.out.println(a/=b);
+}
+}
