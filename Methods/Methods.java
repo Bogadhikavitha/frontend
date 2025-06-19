359f8698f0c835a7bf1a75eae5af2cbd14aa9f00
Bogadhikavitha <Bogadhikavitha@gmail.com>
2025-06-19 11:19:40 +0530

    Add files via upload

---
+++ Methods/Methods.java
@@ -0,0 +1,49 @@
+class Methods
+{
+/*
+public void add()
+{
+System.out.println(10+20);
+}
+static public void main(String[] ab)
+{
+Methods m=new Methods();
+m.add();
+}
+}*/
+/*
+public void sub(int a,int b)
+{
+System.out.println(a-b);
+}
+static public void main(String[] ab)
+{
+Methods m=new Methods();
+m.sub(30,20);
+}
+}
+*/
+/*
+public void mul(int a,int b)
+{
+System.out.println(a*b);
+}
+static public void main(String[] ab)
+{
+Methods m=new Methods();
+m.mul(30,20);
+return;
+}
+
+}
+*/public void div()
+{
+System.out.println(30/20);
+}
+static public void main(String[] ab)
+{
+Methods m=new Methods();
+m.div();
+return;
+}
+}
