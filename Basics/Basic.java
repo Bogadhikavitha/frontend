fb6270b514d83903f43c57f3f259d9f396bffc71
Bogadhikavitha <Bogadhikavitha@gmail.com>
2025-06-20 14:01:47 +0530

    Add files via upload

---
+++ Basics/Basic.java
@@ -0,0 +1,104 @@
+/*
+class Basic
+{
+public void aa()
+{
+System.out.println("kavi");
+}
+public static void main(String args[])
+{  
+Basic b=new Basic();
+b.aa();
+}
+}
+o/p:kavi
+*/
+/* 2
+ class Basic{
+public static void main(String args[])
+{
+System.out.println("My Name is B.kavitha");
+}
+}
+o/p:My Name is B.kavitha
+*/
+/* 3
+class Basic
+{
+/**char()**/
+
+public static void main(String args[])
+{
+/*System.out.println("hi");*/
+//Basic b=new Basic();//
+System.out.println("Ece");
+}
+}
+o/p:Ece */
+/* 4
+class Basics
+{
+    public static void main(String[] args)
+    {
+        int a=24;
+        boolean b=true;
+        char c='A';
+        float d= 3.14f;   
+        double e= 99.99;
+        
+        System.out.println("Integer value: "+a);
+        System.out.println("Boolean value: "+b);
+        System.out.println("Character value: "+c);
+        System.out.println("Float value: " +d);
+        System.out.println("Double value: " +e);
+    }
+}
+Output:
+
+Integer value: 24
+Boolean value: true
+Character value: A
+Float value: 3.14
+Double value: 99.99
+*/
+/* 5
+public class Basics 
+{
+    int x = 10;
+    public void local() {
+    int x = 20;
+ System.out.println("Local variable : " + x);   
+        }
+   public static void main(String[] args) 
+    {
+   Basics b = new Basics();
+   b.local();
+  System.out.println("Global Variable: "+b.x);
+    }
+}
+
+Output:
+Local variable : 20
+Global Variable: 10
+*//*
+
+
+
+class Basics
+{
+    public void name()
+    {
+        System.out.println("kavi");
+    }
+    public static void main(String[] args) 
+    {
+        Basics b=new Basics();
+        b.name();
+    }
+}
+
+Output:
+
+kavi
+
+*/
